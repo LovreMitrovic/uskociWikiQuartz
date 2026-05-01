44f7d9056a31240c6809ee4892026dd7d213c13e
lovre <lovre.mitrovic@gmail.com>
2026-05-01 22:35:56 +0200

    base path fixed

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "lovremitrovic.github.io/uskociWikiQuartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
